cbd1a21a39346560c5527dfbb440f35611be75f1
jestrux <wakyj07@gmail.com>
2017-03-20 23:26:04 +0300

    Advice page

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,20 +11,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(PagesTableSeeder::class);
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(AdvertisementsTableSeeder::class);
-        // $this->call(AdViewsTableSeeder::class);
-        // $this->call(FollowsTableSeeder::class);
-        // $this->call(ProjectsTableSeeder::class);
-        // $this->call(HousesTableSeeder::class);
-        // $this->call(CommentsTableSeeder::class);
-        // $this->call(FavoritesTableSeeder::class);
-        // $this->call(LocationsTableSeeder::class);
+        $this->call(PagesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(AdvertisementsTableSeeder::class);
+        $this->call(AdViewsTableSeeder::class);
+        $this->call(FollowsTableSeeder::class);
+        $this->call(ProjectsTableSeeder::class);
+        $this->call(HousesTableSeeder::class);
+        $this->call(CommentsTableSeeder::class);
+        $this->call(FavoritesTableSeeder::class);
+        $this->call(LocationsTableSeeder::class);
 
-        // $this->call(ArticlesSeeder::class);
-        // $this->call(ArticleCommentsSeeder::class);
-        // $this->call(QuestionsSeeder::class);
-        // $this->call(AnswersSeeder::class);
+        $this->call(ArticlesSeeder::class);
+        $this->call(ArticleCommentsSeeder::class);
+        $this->call(QuestionsSeeder::class);
+        $this->call(AnswersSeeder::class);
     }
 }
